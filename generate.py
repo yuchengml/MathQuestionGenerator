1faef61dd14af009791adbfe8ee7ec479c20e4ea
Yucheng <g1677321@gmail.com>
2024-01-28 15:52:05 +0000

    Implement generating with images

--- generate.py
+++ generate.py
@@ -1,10 +1,12 @@
 import json
 
+from langchain.schema.messages import HumanMessage
 from langchain_community.chat_models import ChatOpenAI
 from langchain_core.output_parsers import JsonOutputParser
 
 from prompt import get_concept_prompt, get_aug_questions_prompt
 from structure import Process, AugmentedQuestions
+from utils import encode_image
 
 
 def generate(q: str = "竹籃裡有24顆蘋果，紅蘋果有6顆，其他是青蘋果，青蘋果有幾顆？"):
@@ -38,5 +40,36 @@ def generate(q: str = "竹籃裡有24顆蘋果，紅蘋果有6顆，其他是青
     print(result)
 
 
+def generate_w_images(q: str = "下圖中的虛線是對稱軸的話，請寫出編號。"):
+    chat = ChatOpenAI(temperature=0,
+                      model="gpt-4-vision-preview",
+                      max_tokens=1024)
+
+    # Path to your image
+    image_path = "ex.jpg"
+
+    # Getting the base64 string
+    base64_image = encode_image(image_path)
+    result = chat.invoke(
+        [
+            HumanMessage(
+                content=[
+                    {"type": "text", "text": "What is this image showing?"},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}",
+                            "detail": "auto",
+                        },
+                    },
+                ]
+            )
+        ]
+    )
+
+    print(result)
+
+
 if __name__ == '__main__':
     generate()
+    generate_w_images()
